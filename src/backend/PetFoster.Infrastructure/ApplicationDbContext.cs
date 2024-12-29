49916d11d484419fb1ea0663a9ac40236ae3bc44
Konstantin Sivkov <kosiv92@gmail.com>
2024-12-29 22:05:26 +0800

    Добавлена строка подключения к БД

--- src/backend/PetFoster.Infrastructure/ApplicationDbContext.cs
+++ src/backend/PetFoster.Infrastructure/ApplicationDbContext.cs
@@ -1,15 +1,18 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using PetFoster.Domain.Entities;
 
 namespace PetFoster.Infrastructure
 {
-    public class ApplicationDbContext: DbContext
+    public class ApplicationDbContext(IConfiguration configuration) : DbContext
     {
+        private const string DATABASE_NAME = "Database";
+
         public DbSet<Volunteer> Volunteers { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseNpgsql();
+            optionsBuilder.UseNpgsql(configuration.GetConnectionString(DATABASE_NAME));
         }
     }
 }
